b95a5f25bef9c476b98d3b94faf9e5f3e401ded3
bsokat <barissokat@gmail.com>
2016-12-13 13:38:43 +0300

    Context and Initializer added

---
+++ DAL/KargoContext.cs
@@ -0,0 +1,33 @@
+﻿using ProjeKargoWebForms.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace ProjeKargoWebForms.DAL
+{
+    public class KargoContext : DbContext
+    {
+        public KargoContext() : base("KargoDB")
+        {
+            Database.SetInitializer(new KargoInitializer());
+        }
+        public DbSet<Adres> Adresler { get; set; }
+        public DbSet<Durum> Durumlar { get; set; }
+        public DbSet<Il> Iller { get; set; }
+        public DbSet<Ilce> Ilceler { get; set; }
+        public DbSet<Kargo> Kargolar { get; set; }
+        public DbSet<Kisi> Kisiler { get; set; }
+        public DbSet<KuryeCagir> KuryeCagirlar { get; set; }
+        public DbSet<Kuryeci> Kuryeciler { get; set; }
+        public DbSet<Sube> Subeler { get; set; }
+        public DbSet<Takip> Takipler { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ DAL/KargoInitializer.cs
@@ -0,0 +1,65 @@
+﻿using ProjeKargoWebForms.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ProjeKargoWebForms.DAL
+{
+    public class KargoInitializer : CreateDatabaseIfNotExists<KargoContext>
+    {
+        protected override void Seed(KargoContext context)
+        {
+            List<Kuryeci> kuryeciler = new List<Kuryeci>
+            {
+                new Kuryeci { Ad = "Cihan", Soyad = "Akpınar" },
+                new Kuryeci { Ad = "Remzi", Soyad = "Bilgi" },
+                new Kuryeci { Ad = "Mehmet", Soyad = "Çelikkol" },
+                new Kuryeci { Ad = "Erhan", Soyad = "Dönmez" },
+                new Kuryeci { Ad = "Gürbüz", Soyad = "Eroğlu" },
+                new Kuryeci { Ad = "Hüseyin", Soyad = "Gökçek" }
+            };
+            kuryeciler.ForEach(kuryeci => context.Kuryeciler.Add(kuryeci));
+            context.SaveChanges();
+
+            List<Ilce> ilceler = new List<Ilce>
+            {
+                new Ilce { IlId = 1, Ad = "Aliağa"},
+                new Ilce { IlId = 1, Ad = "Balçova"},
+                new Ilce { IlId = 1, Ad = "Bayındır"},
+                new Ilce { IlId = 1, Ad = "Bayraklı"},
+                new Ilce { IlId = 1, Ad = "Bergama"},
+                new Ilce { IlId = 1, Ad = "Beydağ"},
+                new Ilce { IlId = 1, Ad = "Bornova"},
+                new Ilce { IlId = 1, Ad = "Buca"},
+                new Ilce { IlId = 1, Ad = "Çeşme"},
+                new Ilce { IlId = 1, Ad = "Çiğli"},
+                new Ilce { IlId = 1, Ad = "Dikili"},
+                new Ilce { IlId = 1, Ad = "Foça"},
+                new Ilce { IlId = 1, Ad = "Gaziemir"},
+                new Ilce { IlId = 1, Ad = "Güzelbahçe"},
+                new Ilce { IlId = 1, Ad = "Karabağlar"},
+                new Ilce { IlId = 1, Ad = "Karaburun"},
+                new Ilce { IlId = 1, Ad = "Karşıyaka"},
+                new Ilce { IlId = 1, Ad = "Kemalpaşa"},
+                new Ilce { IlId = 1, Ad = "Kınık"},
+                new Ilce { IlId = 1, Ad = "Kiraz"},
+                new Ilce { IlId = 1, Ad = "Konak"},
+                new Ilce { IlId = 1, Ad = "Menderes"},
+                new Ilce { IlId = 1, Ad = "Menemen"},
+                new Ilce { IlId = 1, Ad = "Narlıdere"},
+                new Ilce { IlId = 1, Ad = "Ödemiş"},
+                new Ilce { IlId = 1, Ad = "Seferihisar"},
+                new Ilce { IlId = 1, Ad = "Selçuk"},
+                new Ilce { IlId = 1, Ad = "Tire"},
+                new Ilce { IlId = 1, Ad = "Torbalı"},
+                new Ilce { IlId = 1, Ad = "Urla"}
+            };
+            ilceler.ForEach(ilce => context.Ilceler.Add(ilce));
+            context.SaveChanges();
+
+            base.Seed(context);
+        }
+    }
+}
