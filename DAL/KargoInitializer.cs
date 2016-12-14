e0b07e0f1302d3dbf1715da9ba650312f60fe1dd
bsokat <barissokat@gmail.com>
2016-12-14 23:09:24 +0300

    Initializer updated

--- DAL/KargoInitializer.cs
+++ DAL/KargoInitializer.cs
@@ -26,37 +26,58 @@ namespace ProjeKargoWebForms.DAL
             List<Ilce> ilceler = new List<Ilce>
             {
                 new Ilce { IlId = 1, Ad = "Aliağa"},
-                new Ilce { IlId = 1, Ad = "Balçova"},
-                new Ilce { IlId = 1, Ad = "Bayındır"},
-                new Ilce { IlId = 1, Ad = "Bayraklı"},
-                new Ilce { IlId = 1, Ad = "Bergama"},
-                new Ilce { IlId = 1, Ad = "Beydağ"},
                 new Ilce { IlId = 1, Ad = "Bornova"},
-                new Ilce { IlId = 1, Ad = "Buca"},
-                new Ilce { IlId = 1, Ad = "Çeşme"},
                 new Ilce { IlId = 1, Ad = "Çiğli"},
-                new Ilce { IlId = 1, Ad = "Dikili"},
-                new Ilce { IlId = 1, Ad = "Foça"},
                 new Ilce { IlId = 1, Ad = "Gaziemir"},
-                new Ilce { IlId = 1, Ad = "Güzelbahçe"},
-                new Ilce { IlId = 1, Ad = "Karabağlar"},
-                new Ilce { IlId = 1, Ad = "Karaburun"},
                 new Ilce { IlId = 1, Ad = "Karşıyaka"},
-                new Ilce { IlId = 1, Ad = "Kemalpaşa"},
-                new Ilce { IlId = 1, Ad = "Kınık"},
-                new Ilce { IlId = 1, Ad = "Kiraz"},
                 new Ilce { IlId = 1, Ad = "Konak"},
-                new Ilce { IlId = 1, Ad = "Menderes"},
-                new Ilce { IlId = 1, Ad = "Menemen"},
-                new Ilce { IlId = 1, Ad = "Narlıdere"},
-                new Ilce { IlId = 1, Ad = "Ödemiş"},
-                new Ilce { IlId = 1, Ad = "Seferihisar"},
-                new Ilce { IlId = 1, Ad = "Selçuk"},
-                new Ilce { IlId = 1, Ad = "Tire"},
-                new Ilce { IlId = 1, Ad = "Torbalı"},
-                new Ilce { IlId = 1, Ad = "Urla"}
+                new Ilce { IlId = 1, Ad = "Narlıdere"}
             };
             ilceler.ForEach(ilce => context.Ilceler.Add(ilce));
+            context.SaveChanges();
+
+            var izmir = new Il { Ad = "İzmir" };
+            context.Iller.Add(izmir);
+            context.SaveChanges();
+
+            List<Durum> durumlar = new List<Durum>
+            {
+                new Durum { Ad = "Şube1" },
+                new Durum { Ad = "Yolda" },
+                new Durum { Ad = "Şube2" },
+                new Durum { Ad = "Dağıtımda" },
+                new Durum { Ad = "Teslim edildi" }
+            };
+            durumlar.ForEach(durum => context.Durumlar.Add(durum));
+            context.SaveChanges();
+
+            var subeAliagaAdres = new Adres { IlId = 1, IlceId = 1, Mahalle = "Atatürk Mah.", Sokak = "Fatih Sok.", No = "37/D" };
+            var subeBornovaAdres = new Adres { IlId = 1, IlceId = 2, Mahalle = "Kazimdirik Mah.", Sokak = "301 Sok.", No = "41" };
+            var subeCigliAdres = new Adres { IlId = 1, IlceId = 3, Mahalle = "Ataşehir Mah.", Sokak = "8220 Sok.", No = "18/B" };
+            var subeGaziemirAdres = new Adres { IlId = 1, IlceId = 4, Mahalle = "Gazi Mah.", Sokak = "28/25 Sok.", No = "42/A" };
+            var subeKarsiyakaAdres = new Adres { IlId = 1, IlceId = 5, Mahalle = "Bostanlı Mah.", Sokak = "Şehitler Sok.", No = "57/A" };
+            var subeKonakAdres = new Adres { IlId = 1, IlceId = 6, Mahalle = "Halkapinar Mah.", Sokak = "1204/6 Sok.", No = "40/A" };
+            var subeNarlidereAdres = new Adres { IlId = 1, IlceId = 7, Mahalle = "Çamtepe Mah.", Sokak = "Güngören Sok.", No = "43/C" };
+            context.Adresler.Add(subeAliagaAdres);
+            context.Adresler.Add(subeBornovaAdres);
+            context.Adresler.Add(subeCigliAdres);
+            context.Adresler.Add(subeGaziemirAdres);
+            context.Adresler.Add(subeKarsiyakaAdres);
+            context.Adresler.Add(subeKonakAdres);
+            context.Adresler.Add(subeNarlidereAdres);
+            context.SaveChanges();
+
+            List<Sube> subeler = new List<Sube>
+            {
+                new Sube { AdresId = subeAliagaAdres.Id, Ad = "Aliağa Şube", Tel = "0 232 614 6840" },
+                new Sube { AdresId = subeBornovaAdres.Id, Ad = "Bornova Şube", Tel = "0 232 614 6841" },
+                new Sube { AdresId = subeCigliAdres.Id, Ad = "Çiğli Şube", Tel = "0 232 614 6842" },
+                new Sube { AdresId = subeGaziemirAdres.Id, Ad = "Gaziemir Şube", Tel = "0 232 614 6843" },
+                new Sube { AdresId = subeKarsiyakaAdres.Id, Ad = "Karşıyaka Şube", Tel = "0 232 614 6844" },
+                new Sube { AdresId = subeKonakAdres.Id, Ad = "Konak Şube", Tel = "0 232 614 6845" },
+                new Sube { AdresId = subeNarlidereAdres.Id, Ad = "Narlıdere Şube", Tel = "0 232 614 6846" }
+            };
+            subeler.ForEach(sube => context.Subeler.Add(sube));
             context.SaveChanges();
 
             base.Seed(context);
