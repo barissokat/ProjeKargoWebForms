fd63c01d72a765642339db268792da89a941c37f
bsokat <barissokat@gmail.com>
2016-12-23 21:43:06 +0300

    Cargo Tracking added in User page

--- User.aspx
+++ User.aspx
@@ -1,5 +1,35 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="User.aspx.cs" Inherits="ProjeKargoWebForms.User" %>
 
 <asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
-
+    <table style="width:100%;">
+        <tr>
+            <td>
+                <asp:Label ID="Label1" runat="server" Text="Kargom Nerede?" Font-Bold="True" Font-Size="X-Large"></asp:Label>
+            </td>
+            <td class="auto-style1"></td>
+        </tr>
+        <tr>
+            <td>
+                <asp:Label ID="Label2" runat="server" Text="Kargo Takip No:"></asp:Label>
+            </td>
+            <td>
+                <asp:TextBox ID="tbTakipNo" runat="server"></asp:TextBox>
+            </td>
+        </tr>
+        <tr>
+            <td class="auto-style1">&nbsp;</td>
+            <td class="auto-style1">
+                <asp:Button ID="btnKargomNerede" runat="server" Text="Sorgula" OnClick="btnKargomNerede_Click" OnClientClick="return kontrolKargo()" />
+                <asp:Button ID="btnTakipTemizle" runat="server" Text="Temizle" OnClick="btnTakipTemizle_Click" />
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <asp:Label ID="Label3" runat="server" Text="Kargonuz, " Font-Bold="True" Font-Size="Medium"></asp:Label>
+            </td>
+            <td>
+                <asp:Label ID="lblTakipSonuc" runat="server" Font-Bold="True" Font-Size="Medium"></asp:Label>
+            </td>
+        </tr>
+    </table>
 </asp:Content>
--- User.aspx.cs
+++ User.aspx.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ProjeKargoWebForms.DAL;
+using ProjeKargoWebForms.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +11,42 @@ namespace ProjeKargoWebForms
 {
     public partial class User : System.Web.UI.Page
     {
+        private KargoContext db = new KargoContext();
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void btnKargomNerede_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int intControl = new int();
+                var koid = Int32.TryParse(tbTakipNo.Text, out intControl);
+                if (koid == true)
+                {
+                    var tid = Convert.ToInt32(tbTakipNo.Text);
+                    Takip Takip = db.Takipler.Find(tid);
+                    if (Takip != null)
+                    {
+                        lblTakipSonuc.Text = Takip.Durum.Ad.ToString();
+                    }
+                    else
+                        lblTakipSonuc.Text = "Takip numarasını kontrol ediniz.";
+                }
+                else
+                    lblTakipSonuc.Text = "Lütfen rakam giriniz.";
+            }
+            catch (Exception)
+            {
+                lblTakipSonuc.Text = "Lütfen rakam giriniz.";
+            }
+        }
+
+        protected void btnTakipTemizle_Click(object sender, EventArgs e)
+        {
+            tbTakipNo.Text = string.Empty;
+            lblTakipSonuc.Text = string.Empty;
         }
     }
 }
